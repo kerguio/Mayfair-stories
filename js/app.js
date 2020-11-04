2423ade5db683c7f18c8359114e0e62ddd5bdfaa
kerguio <serge@acker.fr>
2020-11-04 14:06:14 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 // SET GLOBAL VARIABLES
 
 // URL of CSV file containing geocoded data 
-var csvurl = "https://raw.githubusercontent.com/kerguio/mapping/main/files/Blue_Plaque_Park_Lane.csv";
+var csvurl = "https://raw.githubusercontent.com/kerguio/Mayfair-stories/main/files/Blue_Plaque_Mayfair.csv";
 var icourl = "img/marker.png"
 
 
